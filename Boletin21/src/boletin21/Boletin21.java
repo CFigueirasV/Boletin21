a9b8eb5f47e1502cb2a25035337b4d6444891933
cfigueirasvalverde <cfigueirasvalverde@nautilus26.danielcastelao.org>
2019-02-11 11:13:57 +0100

    Boletin_21

---
+++ Boletin21/src/boletin21/Boletin21.java
@@ -0,0 +1,20 @@
+package boletin21;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Libreria obx = new Libreria();
+        ArrayList<Libro> l = new ArrayList<>();
+        
+    }
+    
+}
